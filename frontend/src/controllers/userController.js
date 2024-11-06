b4a12d8133f45890a87c7075fe18fe31b3924ed9
miranas11 <anasmir24@gmail.com>
2024-11-06 06:44:25 +0530

    changes

--- frontend/src/controllers/userController.js
+++ frontend/src/controllers/userController.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000";
-
+//const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "http://socialmedia-production-4540.up.railway.app";
 export const registerUser = async (userData) => {
     try {
         const response = await axios.post(
